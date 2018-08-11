229ead91c958a50b617d7f6289db4f8f4803fb45
Passionate-Koda <banjimayowa1@gamil.com>
2018-08-11 02:57:38 +0100

    first one

--- demo_views/admin/add_event.php
+++ demo_views/admin/add_event.php
@@ -1,91 +1,201 @@
 <?php
 ob_start();
 session_start();
-$reda = adminFullInfo($conn,$_SESSION['id']);
-if($reda["bio"] == NULL || $reda["bio"] == ""){
-  header("Location:addProfile");
-}
-include("include/authentication.php");
-// include("include/levelcheck.php");
-authenticate();
 include("include/link_include.php");
+include("include/authentication.php");
+include("include/levelcheck.php");
+include("include/student_limit.php");
+include("include/level1_limit.php");
+authenticate();
+if(isset($_SESSION['id'])){
+  $session = $_SESSION['id'];
+}
+$info = adminInfo($conn,$session);
+extract($info);
+$fname = ucwords($firstname);
+$lname = ucwords($lastname);
 
 
 
+
+
+
+
+$error= [];
+
+if(array_key_exists('submit', $_POST)){
+  if(empty($_POST['name'])){
+    $error['name']="Enter a Name";
+  }
+
+  if(empty($_POST['venue'])){
+    $error['venue']="Enter a Venue";
+  }
+  if(empty($_POST['start_date'])){
+    $error['start_date']="Enter a Start Date";
+  }
+
+  if(empty($_POST['end_date'])){
+    $error['end_date']="Enter a Date";
+  }
+
+  if(empty($_POST['about'])){
+    $error['about']="Enter a About";
+  }
+
+  if(empty($error)){
+    $clean = array_map('trim', $_POST);
+    $firstn = $fname;
+    $lastn = $lname;
+    $uri = explode("/", $_SERVER['REQUEST_URI']);
+    $url = $uri[1];
+     $to = "boardspeck@gmail.com";
+     $subject = "Boardspeck Web Office Content Upload";
+     $txt = "Hello Admin, ($firstn $lastn)has added a content on "."$url"." page at the back office. Kindly check for and approval";
+     $headers = "From: info@boardspeck.com" . "\r\n" .
+     "CC: banjimayowa@gmail.com";
+     mail($to,$subject,$txt,$headers);
+    addEvent($conn, $clean,$hash_id);
+  }
+}
  ?>
+<section id="content">
+<div class="container">
+<div class="row">
+  <?php if (isset($_GET['success'])){
+  $msg = str_replace('_', ' ', $_GET['success']);
 
-    <div id="carousel-area">
+    echo '<div class="col-md-12">
+  <div class="inner-box posting">
+  <div class="alert alert-success alert-lg" role="alert">
+  <h2 class="postin-title">✔ Successful! '.$msg.' </h2>
+  <p>Thank you '.ucwords($firstname).', BoardSpeck Circuit is happy to have you around. </p>
+  </div>
+  </div>
+  </div>';
+  } ?>
+<div class="col-sm-12 col-md-10 col-md-offset-1">
+<div class="page-ads box">
+<h2 class="title-2">Welcome to the Event page</h2>
+<div class="row search-bar mb30 red-bg">
+<div class="advanced-search">
+<form class="search-form" method="get">
+<div class="col-md-4 col-sm-12 search-col">
+<h3>Please post your Event.</h3>
+</div>
+</form>
+</div>
+</div>
+<form class="form-ad" action="" method="post" enctype="multipart/form-data">
+<div class="form-group mb30">
+<label class="control-label">Event Name</label><?php $display = displayErrors($error, 'name');
+echo $display ?> <input class="form-control input-md" name="name" placeholder="Write a suitable title for your Event"  type="text">
+</div>
+<div class="form-group mb30">
+<label class="control-label">Venue</label><?php $display = displayErrors($error, 'venue');
+echo $display ?> <input class="form-control input-md" name="venue" placeholder="Event venue"  type="text">
+<label class="control-label">Start Date</label><?php $display = displayErrors($error, 'start_date');
+echo $display ?> <input class="form-control input-md" name="start_date" placeholder="Start Date"  type="date">
+<label class="control-label">End Date</label><?php $display = displayErrors($error, 'end_date');
+echo $display ?> <input class="form-control input-md" name="end_date" placeholder="End Date"  type="date">
 
+<div class="form-group mb30">
+<label class="control-label" for="textarea">About Event</label>
+<?php $display = displayErrors($error, 'about');
+echo $display ?>
+<textarea class="form-control"  id="editor1" name="about" placeholder="Write about the Event here" rows="4"></textarea>
+</div>
 
-    <div id="carousel-slider" class="carousel slide" data-interval="3000">
-    <div class="carousel-inner">
-
-    <div class="item active" style="background-image: url(images/4544586_ki3_jpeg0bc2afacb2a300932db856c8c85fc7ea.jpg); ">
-    <div class="carousel-caption">
-    <h2 style="text-shadow: 0 0 7px #FD8F2C">
-    Welcome to SkyHeights Academy
-    </h2>
-    <h3 style="text-shadow: 0 0 7px #FD8F2C">
-    THIS IS THE SKYHEIGHTS ACADEMY WEB OFFICE.
-    </h3
-    </div>
-    </div>
-    <div class="item" style="background-image: url(assets/img/slider/slide2.jpg);">
-    <div class="carousel-caption">
-    <h2>
-    POST YOUR CONTENT
-    </h2>
-    <h3>
-    Let the world feel your impact and share of your knowledge
-    </h3>
-    </div>
-    </div>
-    <div class="item" style="background-image: url(assets/img/slider/slide3.jpg);">
-    <div class="carousel-caption">
-    <h2>
-    Get  Exposure
-    </h2>
-    <h3>
-   Launch yourself to the space.
-    </h3>
-    </div>
-    </div>
-    </div>
-    <a class="left carousel-control" href="#carousel-slider" role="button" data-slide="prev">
-    <i class="lnr lnr-chevron-left">
-    </i>
-    </a>
-    <a class="right carousel-control" href="#carousel-slider" role="button" data-slide="next">
-    <i class="lnr lnr-chevron-right">
-    </i>
-    </a>
-    </div>
-    </div>
-
-    </div>
+  <br/>
 
 
+<br/>
+<br/>
+<br/>
+
+<input type="submit" class="btn btn-common" name="submit" value="Add">
+</form>
+</div>
+</div>
 
 
 
+</div>
 
-    <script type="text/javascript" src="assets/js/jquery-min.js"></script>
-    <script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
-    <script type="text/javascript" src="assets/js/material.min.js"></script>
-    <script type="text/javascript" src="assets/js/material-kit.js"></script>
-    <script type="text/javascript" src="assets/js/jquery.parallax.js"></script>
-    <script type="text/javascript" src="assets/js/owl.carousel.min.js"></script>
-    <script type="text/javascript" src="assets/js/wow.js"></script>
-    <script type="text/javascript" src="assets/js/main.js"></script>
-    <script type="text/javascript" src="assets/js/jquery.counterup.min.js"></script>
-    <script type="text/javascript" src="assets/js/waypoints.min.js"></script>
-    <script type="text/javascript" src="assets/js/jasny-bootstrap.min.js"></script>
-    <script type="text/javascript" src="assets/js/form-validator.min.js"></script>
-    <script type="text/javascript" src="assets/js/contact-form-script.js"></script>
-    <script type="text/javascript" src="assets/js/jquery.themepunch.revolution.min.js"></script>
-    <script type="text/javascript" src="assets/js/jquery.themepunch.tools.min.js"></script>
-    <script src="assets/js/bootstrap-select.min.js"></script>
-    </body>
+</div>
+</section>
 
-    <!-- Mirrored from demo.graygrids.com/themes/classix-template/login.html by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 16 Nov 2017 11:40:55 GMT -->
-    </html>
+<a class="back-to-top" href="#"><i class="fa fa-angle-up"></i></a>
+<!-- <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.js"></script>
+<script src="http://netdna.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.js"></script> -->
+
+
+<!-- <script>
+  ClassicEditor
+    .create( document.querySelector( '#editor' ) )
+    .then( editor => {
+      console.log( editor );
+    } )
+    .catch( error => {
+      console.error( error );
+    } );
+</script> -->
+<script type="text/javascript">
+ CKEDITOR.replace( 'editor1',
+ {
+		toolbarGroups :
+		[
+      	{ name: 'clipboard',   groups: [ 'clipboard', 'undo' ] },
+        	{ name: 'editing',     groups: [ 'find', 'selection', 'spellchecker' ] },
+          { name: 'links' },
+            { name: 'insert' },
+              	{ name: 'others' },
+            	{ name: 'forms' },
+            { name: 'tools' },
+            '/',
+            { name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
+            { name: 'paragraph',   groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ] },
+            { name: 'styles' },
+            { name: 'colors' },
+		]
+	});
+</script>
+<script src="assets/js/jquery-min.js" type="text/javascript">
+  </script>
+<script src="assets/js/bootstrap.min.js" type="text/javascript">
+  </script>
+<script src="assets/js/material.min.js" type="text/javascript">
+  </script>
+<script src="assets/js/material-kit.js" type="text/javascript">
+  </script>
+<script src="assets/js/jquery.parallax.js" type="text/javascript">
+  </script>
+<script src="assets/js/owl.carousel.min.js" type="text/javascript">
+  </script>
+<script src="assets/js/wow.js" type="text/javascript">
+  </script>
+<script src="assets/js/main.js" type="text/javascript">
+  </script>
+<script src="assets/js/jquery.counterup.min.js" type="text/javascript">
+  </script>
+<script src="assets/js/waypoints.min.js" type="text/javascript">
+  </script>
+<script src="assets/js/jasny-bootstrap.min.js" type="text/javascript">
+  </script>
+<script src="assets/js/form-validator.min.js" type="text/javascript">
+  </script>
+<script src="assets/js/contact-form-script.js" type="text/javascript">
+  </script>
+<script src="assets/js/jquery.themepunch.revolution.min.js" type="text/javascript">
+  </script>
+<script src="assets/js/jquery.themepunch.tools.min.js" type="text/javascript">
+  </script>
+<script src="assets/js/bootstrap-select.min.js">
+  </script>
+<script src="assets/js/fileinput.js">
+  </script>
+
+</body>
+
+<!-- Mirrored from demo.graygrids.com/themes/classix-template/post-ads.html by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 16 Nov 2017 11:40:57 GMT -->
+</html>
