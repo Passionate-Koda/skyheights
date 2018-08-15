447f1dcc7198d50189a20d887eface8a4788138d
Passionate-Koda <banjimayowa1@gamil.com>
2018-08-15 17:31:05 +0100

    again

--- demo_public_views/contact.php
+++ demo_public_views/contact.php
@@ -12,20 +12,10 @@
   $message = $_POST['comments'];
 
 
-    $txt = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-  <html xmlns="http://www.w3.org/1999/xhtml">
-   <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-    <title>Skyheights Academy</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-  </head>
-  <body>
-    <h1>Message From '.$name.' </h1>
+    $txt = '<h1>Message From '.$name.' </h1>
     <br>
     '.$message.'
-    <br>The email to this message is '.$email.'
-  </body>
-  </html>';
+    <br>The email to this message is '.$email.'';
 
 
 
